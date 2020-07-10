3d913b9450d388e10bccce6c72f8504423b8aed7
Philipp Kaiser <philipp.kaiser@perfectpattern.de>
2020-07-11 00:53:38 +0200

    Reduce example even more

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 #![cfg(target_os = "android")]
 
-
-//////////////////////////////////////////////////
-// Moduls
-
-mod support;
-
-
 //////////////////////////////////////////////////
 // Using
+
+use std::ffi::CStr;
+
+pub mod gl {
+    pub use self::Gles2 as Gl;
+    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+}
 
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoopWindowTarget};
 use glutin::window::WindowBuilder;
 use glutin::{Api, ContextBuilder, GlRequest, PossiblyCurrent, WindowedContext};
-use glutin::platform::android::{AndroidEventLoop, AndroidContextExt};
+use glutin::platform::android::AndroidEventLoop;
 
 
 //////////////////////////////////////////////////
@@ -46,29 +46,41 @@ pub fn main() {
                 },
                 Event::RedrawRequested(_) => {
                     if let Some(context_and_handle) = context_and_handle.as_ref() {
-                        context_and_handle.gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
+                        unsafe { 
+                            context_and_handle.gl.ClearColor(1.0, 0.5, 0.7, 1.0);
+                            context_and_handle.gl.Clear(gl::COLOR_BUFFER_BIT);
+
+                            // do your rendering here
+                        }
                         context_and_handle.context.swap_buffers().unwrap();
                     }
                 },
                 Event::Suspended => {
                     if context_and_handle.is_some() && ndk_glue::native_window().is_some() {
+
+                        // destroy your graphics resources here
+
                         context_and_handle = None;
                     }
                 },
                 Event::Resumed => {
                     if context_and_handle.is_none() && ndk_glue::native_window().is_some() {
                         context_and_handle = Some(init_context(el));
+
+                        // create your graphics resources here
                     }
                 },
                 _ => (),
             }
         });
+
+        // update your game here
     }
 }
 
 struct ContextAndHandle {
     context: WindowedContext<PossiblyCurrent>,
-    gl: support::Gl,
+    gl: gl::Gl,
 }
 
 fn init_context(el: &EventLoopWindowTarget<()>) -> ContextAndHandle {
@@ -83,7 +95,17 @@ fn init_context(el: &EventLoopWindowTarget<()>) -> ContextAndHandle {
     let context = unsafe { context.make_current().unwrap() };
 
     println!("Pixel format of the window's GL context: {:?}", context.get_pixel_format());
-    let gl = support::load(&context.context());
+
+    let gl = gl::Gl::load_with(|ptr| context.get_proc_address(ptr) as *const _);
+
+    let version = unsafe {
+        let data = CStr::from_ptr(gl.GetString(gl::VERSION) as *const _)
+            .to_bytes()
+            .to_vec();
+        String::from_utf8(data).unwrap()
+    };
+
+    println!("OpenGL version {}", version);
 
     ContextAndHandle { context, gl }
 }
