4cb2b3bf18671fc59cc367795f8a69cc87795106
Philipp Kaiser <philippxkaiser@googlemail.com>
2022-06-30 23:37:01 +0200

    Formatting

--- src/runner.rs
+++ src/runner.rs
@@ -19,19 +19,13 @@ pub fn start() {
     let log_level = log::LevelFilter::Debug;
     #[cfg(not(debug_assertions))]
     let log_level = log::LevelFilter::Info;
-    match SimpleLogger::new()
-        .with_utc_timestamps()
-        .with_level(log_level)
-        .init()
-    {
+    match SimpleLogger::new().with_utc_timestamps().with_level(log_level).init() {
         Err(s) => println!("{}", s),
         _ => {}
     }
 
     // init game loop and run
-    let mut game_loop = GameLoop::new(ExampleRunner {
-        ..Default::default()
-    });
+    let mut game_loop = GameLoop::new(ExampleRunner { ..Default::default() });
     game_loop.run();
 }
 
@@ -91,31 +85,29 @@ impl Runner for ExampleRunner {
     fn update(&mut self, _elapsed_time: f32) {}
 
     fn input(&mut self, input_events: &[InputEvent]) {
-        input_events
-            .iter()
-            .for_each(|input_event| match input_event {
-                InputEvent::Cursor(event) => {
-                    println!("{:?}", event);
+        input_events.iter().for_each(|input_event| match input_event {
+            InputEvent::Cursor(event) => {
+                println!("{:?}", event);
+            }
+            InputEvent::Mouse(event) => {
+                println!("{:?}", event);
+            }
+            InputEvent::Touch(event) => {
+                println!("{:?}", event);
+            }
+            InputEvent::Keyboard(KeyboardEvent { state, key }) => match (state, key) {
+                (KeyState::Released, Key::Back) => {
+                    #[cfg(target_os = "android")]
+                    ndk_glue::native_activity().finish();
+                    #[cfg(not(target_os = "android"))]
+                    std::process::exit(0);
                 }
-                InputEvent::Mouse(event) => {
-                    println!("{:?}", event);
+                (KeyState::Pressed, Key::Unknown) => {
+                    log::debug!("Unkown key pressed, complete key list!");
                 }
-                InputEvent::Touch(event) => {
-                    println!("{:?}", event);
-                }
-                InputEvent::Keyboard(KeyboardEvent { state, key }) => match (state, key) {
-                    (KeyState::Released, Key::Back) => {
-                        #[cfg(target_os = "android")]
-                        ndk_glue::native_activity().finish();
-                        #[cfg(not(target_os = "android"))]
-                        std::process::exit(0);
-                    }
-                    (KeyState::Pressed, Key::Unknown) => {
-                        log::debug!("Unkown key pressed, complete key list!");
-                    }
-                    _ => {}
-                },
-            });
+                _ => {}
+            },
+        });
     }
 
     fn render(&mut self, gl: &Gl) {
@@ -141,8 +133,7 @@ impl Runner for ExampleRunner {
             // gl.DepthMask(gl::TRUE);
             // gl.DepthFunc(gl::LESS);
 
-            self.shader
-                .draw_elements(gl::TRIANGLE_STRIP, self.ibo.count());
+            self.shader.draw_elements(gl::TRIANGLE_STRIP, self.ibo.count());
 
             self.shader.unbind();
 
@@ -161,12 +152,7 @@ impl Runner for ExampleRunner {
         self.vao = GlVertexArrayObject::new(gl);
 
         self.vbo = GlVertexBuffer::new(gl, gl::STATIC_DRAW, &[[0.0; 4]; 4]);
-        self.vbo.update(&[
-            [-0.5, -0.5, 0.0, 1.0],
-            [-0.5, 0.5, 0.0, 0.0],
-            [0.5, -0.5, 1.0, 1.0],
-            [0.5, 0.5, 1.0, 0.0],
-        ]);
+        self.vbo.update(&[[-0.5, -0.5, 0.0, 1.0], [-0.5, 0.5, 0.0, 0.0], [0.5, -0.5, 1.0, 1.0], [0.5, 0.5, 1.0, 0.0]]);
 
         self.ibo = GlIndexBuffer::new(gl, gl::STATIC_DRAW, &[0; 4]);
         self.ibo.update(&[0, 1, 2, 3]);
@@ -174,35 +160,15 @@ impl Runner for ExampleRunner {
         self.ubo = GlUniformBuffer::new(gl, gl::DYNAMIC_DRAW, &(0.0, 0.0, 0.0, 0.0));
         self.ubo.update(&(0.5, 0.9, 0.9, 1.0));
 
-        let image = image::load_from_memory(&File::load_bytes("lena.png").unwrap())
-            .unwrap()
-            .to_rgba8();
+        let image = image::load_from_memory(&File::load_bytes("lena.png").unwrap()).unwrap().to_rgba8();
         self.texture = GlTexture::new(gl, &[image]);
 
         self.shader = GlShader::new(gl, VS, FS);
 
         // bind buffers to vao
         self.vao.bind();
-        self.vao.bind_attrib(
-            &self.vbo,
-            0,
-            2,
-            gl::FLOAT,
-            gl::FALSE,
-            0,
-            4 * size_of::<f32>(),
-            0,
-        );
-        self.vao.bind_attrib(
-            &self.vbo,
-            1,
-            2,
-            gl::FLOAT,
-            gl::FALSE,
-            2 * size_of::<f32>(),
-            4 * size_of::<f32>(),
-            0,
-        );
+        self.vao.bind_attrib(&self.vbo, 0, 2, gl::FLOAT, gl::FALSE, 0, 4 * size_of::<f32>(), 0);
+        self.vao.bind_attrib(&self.vbo, 1, 2, gl::FLOAT, gl::FALSE, 2 * size_of::<f32>(), 4 * size_of::<f32>(), 0);
         self.vao.unbind();
     }
 
